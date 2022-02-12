0b4b919a048e0ce781eecde2f733713b603ac598
tyago343 <tyagocasanova@gmail.com>
2022-02-12 16:54:05 +0100

    hotfix

--- src/App.js
+++ src/App.js
@@ -20,8 +20,10 @@ const App = () => {
     for (let i = 0; i <= 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
       const decidedColor = currentColorArrangement[i];
-      if (columnOfThree.every((square) => currentColorArrangement[square] === decidedColor)) {
-        setScoreDisplay((score) => score + 3)
+      const isBlank = currentColorArrangement[i] === blank;
+
+      if (columnOfThree.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 3);
         columnOfThree.forEach((square) => (currentColorArrangement[square] = blank));
         return true;
       }
@@ -31,8 +33,11 @@ const App = () => {
     for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
       const decidedColor = currentColorArrangement[i];
-      if (columnOfFour.every((square) => currentColorArrangement[square] === decidedColor)) {
-        setScoreDisplay((score) => score + 4)
+      const isBlank = currentColorArrangement[i] === blank;
+      if (
+        columnOfFour.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)
+      ) {
+        setScoreDisplay((score) => score + 4);
 
         columnOfFour.forEach((square) => (currentColorArrangement[square] = blank));
         return true;
@@ -44,10 +49,12 @@ const App = () => {
       const rowOfThree = [i, i + 1, i + 2];
       const decidedColor = currentColorArrangement[i];
       const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64];
+      const isBlank = currentColorArrangement[i] === blank;
+
       if (notValid.includes(i)) continue;
 
-      if (rowOfThree.every((square) => currentColorArrangement[square] === decidedColor)) {
-        setScoreDisplay((score) => score + 3)
+      if (rowOfThree.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 3);
 
         rowOfThree.forEach((square) => (currentColorArrangement[square] = blank));
         return true;
@@ -61,11 +68,12 @@ const App = () => {
       const notValid = [
         5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 63, 64,
       ];
+      const isBlank = currentColorArrangement[i] === blank;
 
       if (notValid.includes(i)) continue;
 
-      if (rowOfFour.every((square) => currentColorArrangement[square] === decidedColor)) {
-        setScoreDisplay((score) => score + 4)
+      if (rowOfFour.every((square) => currentColorArrangement[square] === decidedColor && !isBlank)) {
+        setScoreDisplay((score) => score + 4);
 
         rowOfFour.forEach((square) => (currentColorArrangement[square] = blank));
         return true;
